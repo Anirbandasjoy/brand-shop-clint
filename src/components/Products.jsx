4c7a5802ac85d58357857562a2165c2c0d26c4ba
joydas <joy600508@gmail.com>
2023-10-20 03:55:39 +0600

    modify my product card

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -66,17 +66,17 @@ const Products = () => {
                 {
                     products.map((product) => {
                         return <div key={product._id} className="w-full  bg-white border border-gray-200 rounded-lg shadow ">
-                            <Link>
+                            <div>
                                 <img className=" h-96 w-full rounded-t-lg" src={product.image} alt={product.name} />
-                            </Link>
+                            </div>
                             <div className="px-5 pb-5 mt-4">
-                                <Link>
+                                <div>
                                     <div className="flex  gap-2 ">
                                         <h5 className="text-2xl font-semibold tracking-tight text-gray-900 ">{product.name}</h5>
                                         <h4 className="text-xs font-semibold bg-primary h-fit text-white p-0.5 px-1 rounded-sm ">{product.category}</h4>
                                     </div>
                                     <div className="badge badge-primary rounded-sm">{product.brand}</div>
-                                </Link>
+                                </div>
                                 <div className="flex text-yellow-600 items-center mt-2.5 mb-5">
                                     <Rating
                                         initialRating={product.rating}
